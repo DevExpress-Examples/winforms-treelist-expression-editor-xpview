2e641e2447eaa83c5d7b55278c505b59cb9d4428
DevExpressExampleBot <fakeEmail>
2023-09-24 10:59:52 +0400

    Source auto update [skip ci]

--- VB/E1887/PersistentClasses.vb
+++ VB/E1887/PersistentClasses.vb
@@ -1,59 +1,68 @@
-Imports Microsoft.VisualBasic
-Imports System
 Imports DevExpress.Xpo
+
 Namespace DXSample.BO
-	Public Class Order
-		Inherits XPObject
-		Public Sub New(ByVal session As Session)
-			MyBase.New(session)
-		End Sub
 
-		Private fName As String
-		Public Property Name() As String
-			Get
-				Return fName
-			End Get
-			Set(ByVal value As String)
-				SetPropertyValue(Of String)("Name", fName, value)
-			End Set
-		End Property
+    Public Class Order
+        Inherits XPObject
 
-		Private fUnitPrice As Decimal
-		Public Property UnitPrice() As Decimal
-			Get
-				Return fUnitPrice
-			End Get
-			Set(ByVal value As Decimal)
-				SetPropertyValue(Of Decimal)("UnitPrice", fUnitPrice, value)
-			End Set
-		End Property
+        Public Sub New(ByVal session As Session)
+            MyBase.New(session)
+        End Sub
 
-		Private fQuantity As Integer
-		Public Property Quantity() As Integer
-			Get
-				Return fQuantity
-			End Get
-			Set(ByVal value As Integer)
-				SetPropertyValue(Of Integer)("Quantity", fQuantity, value)
-			End Set
-		End Property
+        Private fName As String
 
-		Private fParent As Order
-		<Association("Order-SubOrders")> _
-		Public Property Parent() As Order
-			Get
-				Return fParent
-			End Get
-			Set(ByVal value As Order)
-				SetPropertyValue(Of Order)("Parent", fParent, value)
-			End Set
-		End Property
+        Public Property Name As String
+            Get
+                Return fName
+            End Get
 
-		<Association("Order-SubOrders", GetType(Order))> _
-		Public ReadOnly Property SubOrders() As XPCollection(Of Order)
-			Get
-				Return GetCollection(Of Order)("SubOrders")
-			End Get
-		End Property
-	End Class
+            Set(ByVal value As String)
+                SetPropertyValue("Name", fName, value)
+            End Set
+        End Property
+
+        Private fUnitPrice As Decimal
+
+        Public Property UnitPrice As Decimal
+            Get
+                Return fUnitPrice
+            End Get
+
+            Set(ByVal value As Decimal)
+                SetPropertyValue(Of Decimal)("UnitPrice", fUnitPrice, value)
+            End Set
+        End Property
+
+        Private fQuantity As Integer
+
+        Public Property Quantity As Integer
+            Get
+                Return fQuantity
+            End Get
+
+            Set(ByVal value As Integer)
+                SetPropertyValue(Of Integer)("Quantity", fQuantity, value)
+            End Set
+        End Property
+
+        Private fParent As Order
+
+        <Association("Order-SubOrders")>
+        Public Property Parent As Order
+            Get
+                Return fParent
+            End Get
+
+            Set(ByVal value As Order)
+                SetPropertyValue("Parent", fParent, value)
+            End Set
+        End Property
+
+        <Association("Order-SubOrders", GetType(Order))>
+        Public ReadOnly Property SubOrders As XPCollection(Of Order)
+            Get
+                Return GetCollection(Of Order)("SubOrders")
+            End Get
+        End Property
+    End Class
 End Namespace
